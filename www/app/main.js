d947222bc0418272c669354a59528c1591e2dac9
Phan Quoc Huy <quochuy.phan87@gmail.com>
2017-03-20 09:15:30 +0000

    initial commit

---
+++ www/app/main.js
@@ -0,0 +1,15 @@
+define(function (require) {
+	'use strict';
+	
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    var messages = require('./messages');
+
+    // Load library/vendor modules using
+    // full IDs, like:
+    var print = require('print');
+    print(messages.getHello());
+
+    var test = require('./test');
+});
